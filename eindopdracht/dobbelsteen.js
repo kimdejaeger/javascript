f34d35b449f8645de472bd24a2e69ac0140e5bcd
kim <kf.dejaeger@gmail.com>
2026-02-17 20:22:20 +0100

    Oplossing met for loop

--- eindopdracht/dobbelsteen.js
+++ eindopdracht/dobbelsteen.js
@@ -1,24 +1,40 @@
 let gooien = document.getElementById("gooi");
 gooien.addEventListener("click", gooiDobbelsteen);
 
-let dobbelsteenPlaatjes = [
-  "images/dice1.png",
-  "images/dice2.png",
-  "images/dice3.png",
-  "images/dice4.png",
-  "images/dice5.png",
-  "images/dice6.png",
-];
+// let dobbelsteenPlaatjes = [
+//   "images/dice1.png",
+//   "images/dice2.png",
+//   "images/dice3.png",
+//   "images/dice4.png",
+//   "images/dice5.png",
+//   "images/dice6.png",
+// ];
 
 function gooiDobbelsteen() {
-  const diceDivs = document.querySelectorAll(".dice");
 
-  diceDivs.forEach((div) => {
-    const randomIndex = Math.floor(Math.random() * dobbelsteenPlaatjes.length);
+  for (let i = 1; i <= 4; i++) {
+    let aantalOgen = Math.floor(Math.random() * 6) + 1;
+    console.log(aantalOgen);
+    let diceDiv = "dice" + i;
 
+    console.log(diceDiv);
+    let diceImage = "images/dice" + aantalOgen + ".png";
+    console.log(diceImage);
+
+    let diceDivs = document.getElementById(diceDiv);
     const img = document.createElement("img");
-    img.src = dobbelsteenPlaatjes[randomIndex];
+    img.src = diceImage;
+    diceDivs.replaceChildren(img);
+  }
 
-    div.replaceChildren(img);
-  });
+  //   const diceDivs = document.querySelectorAll(".dice");
+
+  //   diceDivs.forEach((div) => {
+  //     const randomIndex = Math.floor(Math.random() * dobbelsteenPlaatjes.length);
+
+  //     const img = document.createElement("img");
+  //     img.src = dobbelsteenPlaatjes[randomIndex];
+
+  //     div.replaceChildren(img);
+  //   });
 }
